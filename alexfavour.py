2e5c43bf950f2efdf39625fa02318bafa8e8bf1d
Codeindeed <favouralex084@gmail.com>
2023-01-16 14:51:17 +0100

    First IT for 300lvl

---
+++ alexfavour.py
@@ -0,0 +1,41 @@
+#Name: Alex Favour CHukwuebuka
+#REG NO : 20181113203
+#Department : Electrical and Electronics Engineering
+#SIWES TYPE : PYTHON
+#SIWES CLASS : 300
+#UCC NO : 23/
+
+
+
+
+#QUESTION NO 1A
+#ANSWER:
+#1 : WEB DEVELOPMENT :  Python language can be used in writing code for building websites and also automating things on the web 
+#2 : DATA SCIENCE : Python can be used in scrapping data that would be used for data analytics in data science.
+#3 : MACHINE LEARNING : Python can be used in writing code for creating models in machine learning 
+
+
+# QUESTION 1.B
+WeightPrompt = float(input('Enter YOUR WEIGHT IN POUNDS :'))
+weight = WeightPrompt * 0.4539237
+HeightPrompt = float(input('ENTER YOUR HEIGHT IN INCHES :'))
+height = HeightPrompt * 0.0254
+BMI = weight /(height **2)
+
+print(f'BMI is {BMI}')
+
+print('-----------------QUESTION NO 2 STARTS HERE---------------')
+
+#QUESTION NO 2A
+#ANSWER:
+#A Compiler is a program that converts low level language to a high level language
+#A Interpreter converts a low level language to a high level language and then which is moved to a machine readable format
+
+#QUESTION NO 2B
+        
+vo,v1,t = (float(input('vo:')),float(input('v1:')), float(input('t:')))
+a = (v1 - vo) / t
+
+print(f'The average acceleration is {a}')
+
+
